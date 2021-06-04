3e82fd96ae723bd74edae10281a1215550dc64ed
Christopher <christophermelendez13@gmail.com>
2021-06-03 19:24:05 -0600

    Vista Home realizada

--- tickets-front/src/store/index.js
+++ tickets-front/src/store/index.js
@@ -1,15 +1,29 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: {
+		tickets: [],
+		loading: false,
+	},
+	mutations: {
+		SET_TICKETS(state, tickets) {
+			state.tickets = tickets;
+		},
+		SET_LOADING(state, payload) {
+			state.loading = payload;
+		},
+	},
+	actions: {
+		setTickets({ commit }) {
+			axios.get("http://localhost:3000/ticket").then((response) => {
+				commit("SET_TICKETS", response.data).finally(() =>
+					commit("SET_LOADING", false)
+				);
+			});
+		},
+	},
+	modules: {},
+});
--- tickets-front/src/views/Home.vue
+++ tickets-front/src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<template>
+			<div>
+				<b-table
+					striped
+					hover
+					:items="tickets"
+					:fields="campos"
+					:tbody-tr-class="rowClass"
+				></b-table>
+			</div>
+		</template>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapState, mapActions } from "vuex";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "Home",
+	components: {},
+	computed: {
+		...mapState(["tickets", "loading"]),
+	},
+	methods: {
+		...mapActions(["setTickets"]),
+		rowClass(item, type) {
+			if (!item || type !== "row") return;
+			if (item.ticket_estatus === "ABT") return "table-danger";
+			if (item.ticket_estatus === "ESP") return "table-success";
+			if (item.ticket_estatus === "FIN") return "table-dark";
+		},
+	},
+
+	data() {
+		return {
+			// Note 'isActive' is left out and will not appear in the rendered table
+			campos: [
+				{
+					key: "ticket_nombre",
+					label: "Nombre del ticket",
+					sortable: false,
+				},
+				{
+					key: "categoria_nombre",
+					label: "Categoría",
+					sortable: true,
+				},
+			],
+		};
+	},
+	mounted() {
+		this.setTickets();
+	},
+};
 </script>
+
+<style>
+.sr-only {
+	display: none;
+}
+</style>
